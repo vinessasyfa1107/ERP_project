aed591188ae0d4b6d9361f786f05feb9c5835973
Arif Suwadji <arifsuwadji@gmail.com>
2023-10-02 11:13:40 +0700

    Perbaikan konfigurasi unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
-import unocssPlugin from "unocss/vite";
+import UnoCSS from "unocss/vite";
 
 export default defineConfig({
   plugins: [
@@ -11,7 +11,9 @@ export default defineConfig({
     */
     // devtools(),
     solidPlugin(),
-    unocssPlugin()
+    UnoCSS({
+      configFile: './uno.config.ts',
+    })
   ],
   server: {
     port: 3000,
