7974f4ef0a571b94d61a20ab0cb38a6faf22aa10
Zuhrah Zulfaa <zuhrahzlf@gmail.com>
2023-10-09 16:50:43 +0700

    membuat pop up untuk pt

---
+++ src/containers/navbars/pop-up/pt-pop-up.tsx
@@ -0,0 +1,21 @@
+import type { Component } from 'solid-js';
+import './pt-pop-up.css'
+
+const PT_PopUp: Component = () => {
+  return (
+    <div>
+        <div id="slide" class="pt-pop-up">
+            <button>PT. astra A</button>
+            <button>PT. astra A</button>
+            <button>PT. astra A</button>
+            <button>PT. astra A</button>
+            <button>PT. astra A</button>
+            <button>PT. astra A</button>
+            <button>PT. astra A</button>
+            <button>PT. astra A</button>
+        </div>
+    </div>
+  );
+};
+
+export default PT_PopUp;
