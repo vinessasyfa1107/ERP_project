2d29d10255096a8b92c709b37a84290ed91f597c
Vinessa Gabby Asyfa <vgabbyasyfa1107@gmail.com>
2023-10-13 18:01:25 +0700

    membuat filter tagging pada tabel, evidence, dan button tutup buku sudah selesai

--- src/containers/report/pemasukan/pemasukan.tsx
+++ src/containers/report/pemasukan/pemasukan.tsx
@@ -1,6 +1,7 @@
 import type { Component } from 'solid-js';
 import AgGridSolid from 'ag-grid-solid';
-
+import { Grid, GridOptions, ISetFilterParams } from 'ag-grid-community';
+import 'ag-grid-enterprise';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import 'daisyui/dist/full.css';
@@ -26,89 +27,169 @@ const Pemasukan: Component = () => {
     );
   };
 
-  const columnDefs = [
-    { headerName: 'ID', field: 'id' },
-    { headerName: 'ID Pengajuan', field: 'id_pengajuan' },
-    { headerName: 'Jumlah', field: 'jumlah' },
-    { headerName: 'Tanggal', field: 'tanggal' },
-    { headerName: 'Keterangan', field: 'keterangan' },
-    { headerName: 'Bukti', field: 'bukti', cellRenderer: agLinkCellRenderer },
-    {
-      headerName: 'Tags',
-      field: 'tags',
-      filter: 'agSetColumnFilter',
-      filterParams: {
-        values: [] // Nilai-nilai unik akan diisi secara otomatis 
-      }
+  const tagsCellRenderer = (params: any) => {
+    const tags = params.value;
+    let tagColor = 'transparent';
+
+    switch (tags) {
+      case 'VIP':
+        tagColor = '#F56D59'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'In Progress':
+        tagColor = '#C7C7C7'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'Urgent':
+        tagColor = '#040404'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'Bug':
+        tagColor = '#31DFEA'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'VVIP':
+        tagColor = '#FF2100'; // Ganti dengan warna yang Anda inginkan
+        break;
+      default:
+        tagColor = 'transparent'; // Warna default untuk nilai lain
     }
-  ];
 
-  const rowData = [
-    {
-      "id": 1,
-      "id_pengajuan": 123,
-      "jumlah": 2000000,
-      "tanggal": '10/12/22',
-      "keterangan": "Lorem Ipsum Dolor Sit Amet",
-      "bukti": "evidance.jpg",
-      "tags": "VIP"
-    },
-  ];
-
-  const defaultColDef = {
-    flex: 1,
-    filter: 'agTextColumnFilter',
+    return (
+      <div>
+        <span
+          style={{
+            "background-color": tagColor,
+            "border-radius": '50%',
+            "display": 'inline-block',
+            "width": '10px',
+            "height": '10px',
+            "margin-right": '8px',
+          }}
+        ></span>
+        {tags}
+      </div>
+    );
   };
 
-  const gridOptions = {
-    pagination: true,
-    paginationPageSize: 4,
-    rowHeight: 40,
-    // frameworkComponents, // Tambahkan frameworkComponents ke dalam gridOptions
-  };
 
-  return (
-    <div>
-      <Semua_laporanNavbar />
-      <div class="pemasukan-container">
-        {/* div untuk mengatur top table pada pemasukan yang terdiri dari judul tabel, search, dan sorting icon */}
-        <div class="pemasukan-top-table">
-          <div class="tutup-buku-container">
-            <Tutup_buku />
-          </div>
+const columnDefs = [
+  { headerName: 'ID', field: 'id' },
+  { headerName: 'ID Pengajuan', field: 'id_pengajuan' },
+  { headerName: 'Jumlah', field: 'jumlah' },
+  { headerName: 'Tanggal', field: 'tanggal' },
+  { headerName: 'Keterangan', field: 'keterangan' },
+  { headerName: 'Bukti', field: 'bukti', cellRenderer: agLinkCellRenderer },
+  {
+    headerName: 'Tags',
+    field: 'tags',
+    filter: 'agSetColumnFilter',
+    filterParams: {
+      applyMiniFilterWhileTyping: true,
+      values: ['VIP','In Progress','Urgent','Bug','VVIP'],
+    } as ISetFilterParams,
+    cellRenderer: tagsCellRenderer
+  }
+];
 
-          <div class="search-container">
-            <div class="search-input">
-              <input
-                type="text"
-                class="form-control"
-                id="filter-text-box"
-                placeholder="Search..."
-              />
-              <span class="search-icon">
-                <Icon icon="ic:baseline-search" color="gray" width="16" height="16" />
-              </span>
-            </div>
-            <button class="btn-sort"><Icon icon="gg:sort-za" color="white" width="25" height="25" /></button>
-          </div>
+const rowData = [
+  {
+    "id": 1,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "VIP"
+  },
+  {
+    "id": 2,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "In Progress"
+  },
+  {
+    "id": 3,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "Urgent"
+  },
+  {
+    "id": 4,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "Bug"
+  },
+  {
+    "id": 5,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "VVIP"
+  }
+];
+
+const defaultColDef = {
+  flex: 1,
+  filter: 'agTextColumnFilter',
+};
+
+const gridOptions = {
+  pagination: true,
+  paginationPageSize: 5,
+  rowHeight: 40,
+  // frameworkComponents, // Tambahkan frameworkComponents ke dalam gridOptions
+};
+
+return (
+  <div>
+    <Semua_laporanNavbar />
+    <div class="pemasukan-container">
+      {/* div untuk mengatur top table pada pemasukan yang terdiri dari judul tabel, search, dan sorting icon */}
+      <div class="pemasukan-top-table">
+        <div class="tutup-buku-container">
+          <Tutup_buku />
         </div>
-        <p>Laporan Pemasukan</p>
 
-        {/* div untuk mengatur tabel kas besar dari ag grid */}
-        <div class="pemasukan-table">
-          <div class="ag-theme-alpine" style={{ width: '68vw' }}>
-            <AgGridSolid
-              columnDefs={columnDefs}
-              rowData={rowData}
-              defaultColDef={defaultColDef}
-              domLayout='autoHeight'
-              gridOptions={gridOptions}
+        <div class="search-container">
+          <div class="search-input">
+            <input
+              type="text"
+              class="form-control"
+              id="filter-text-box"
+              placeholder="Search..."
             />
+            <span class="search-icon">
+              <Icon icon="ic:baseline-search" color="gray" width="16" height="16" />
+            </span>
           </div>
+          <button class="btn-sort"><Icon icon="gg:sort-za" color="white" width="25" height="25" /></button>
+        </div>
+      </div>
+      <p>Laporan Pemasukan</p>
+
+      {/* div untuk mengatur tabel kas besar dari ag grid */}
+      <div class="pemasukan-table">
+        <div class="ag-theme-alpine" style={{ width: '68vw' }}>
+          <AgGridSolid
+            columnDefs={columnDefs}
+            rowData={rowData}
+            defaultColDef={defaultColDef}
+            domLayout='autoHeight'
+            gridOptions={gridOptions}
+          />
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default Pemasukan;
--- src/containers/report/pengeluaran/pengeluaran.tsx
+++ src/containers/report/pengeluaran/pengeluaran.tsx
@@ -1,92 +1,195 @@
 import type { Component } from 'solid-js';
 import AgGridSolid from 'ag-grid-solid';
+import { Grid, GridOptions, ISetFilterParams } from 'ag-grid-community';
+import 'ag-grid-enterprise';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import 'daisyui/dist/full.css';
 import { Icon } from '@iconify-icon/solid';
-import { createEffect, createSignal } from 'solid-js';
+import { ColDef, MenuItemDef } from 'ag-grid-enterprise';
+
+
 import Semua_laporanNavbar from '../semua_laporanNavbar';
 import './pengeluaran.css';
+import Tutup_buku from '../pemasukan/tutup_buku';
+
 
 const Pengeluaran: Component = () => {
-  const columnDefs = [
-    { headerName: 'ID', field: 'id' },
-    { headerName: 'ID Pengajuan', field: 'id_pengajuan' },
-    { headerName: 'Jumlah', field: 'jumlah' },
-    { headerName: 'Tanggal', field: 'tanggal' },
-    { headerName: 'Keterangan', field: 'keterangan' },
-    { headerName: 'Bukti', field: 'bukti' },
-    { headerName: 'Tags', field: 'tags' }
-  ];
 
-  const rowData = [
-    {
-      "id": 1,
-      "id_pengajuan": 123,
-      "jumlah": 2000000,
-      "tanggal": '10/12/22',
-      "keterangan": "Lorem Ipsum Dolor Sit Amet",
-      "bukti": "evidance.jpg",
-      "tags": "VIP"
-    },
-  ];
+  const agLinkCellRenderer = (params: any) => {
+    const bukti = params.data.bukti;
+    const link = `https://example.com/city/${bukti}`;
 
-  const defaultColDef = {
-    flex: 1,
-    filter: 'agTextColumnFilter',
+    return (
+      <a href={link} target="_blank" rel="noopener noreferrer" style={{ "text-decoration": "underline", color: "blue" }}>
+        {bukti}
+      </a>
+    );
   };
 
-  const gridOptions = {
-    pagination: true,
-    paginationPageSize: 4,
-    rowHeight: 40
-  };
+  const tagsCellRenderer = (params: any) => {
+    const tags = params.value;
+    let tagColor = 'transparent';
 
+    switch (tags) {
+      case 'VIP':
+        tagColor = '#F56D59'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'In Progress':
+        tagColor = '#C7C7C7'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'Urgent':
+        tagColor = '#040404'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'Bug':
+        tagColor = '#31DFEA'; // Ganti dengan warna yang Anda inginkan
+        break;
+      case 'VVIP':
+        tagColor = '#FF2100'; // Ganti dengan warna yang Anda inginkan
+        break;
+      default:
+        tagColor = 'transparent'; // Warna default untuk nilai lain
+    }
 
-  return (
-    <div>
-      <Semua_laporanNavbar />
-      <div class="pengeluaran-container" >
-        {/* div untuk mengatur top table pada pemasukan yang terdiri dari judul tabel, search, dan sorting icon */}
-        <div class="pengeluaran-top-table">
-          <div class="tutup-buku-container">
-            <button>+ Tutup Buku</button>
-          </div>
-
-          <div class="search-container">
-            <div class="search-input">
-              <input
-                type="text"
-                class="form-control"
-                id="filter-text-box"
-                placeholder="Search..."
-              />
-              <span class="search-icon">
-                <Icon icon="ic:baseline-search" color="gray" width="16" height="16" />
-              </span>
-            </div>
-            <button class="btn-sort"><Icon icon="gg:sort-za" color="white" width="25" height="25" /></button>
-          </div>
-        </div>
-        <p>Laporan Pengeluaran</p>
-
-        {/* div untuk mengatur tabel kas besar dari ag grid */}
-        <div class="pengeluaran-table">
-          <div class="ag-theme-alpine" style={{ width: '68vw' }}>
-            <AgGridSolid
-              columnDefs={columnDefs}
-              rowData={rowData}
-              defaultColDef={defaultColDef}
-              domLayout='autoHeight'
-              gridOptions={gridOptions}
-            />
-          </div>
-        </div>
-
+    return (
+      <div>
+        <span
+          style={{
+            "background-color": tagColor,
+            "border-radius": '50%',
+            "display": 'inline-block',
+            "width": '10px',
+            "height": '10px',
+            "margin-right": '8px',
+          }}
+        ></span>
+        {tags}
       </div>
+    );
+  };
 
+
+const columnDefs = [
+  { headerName: 'ID', field: 'id' },
+  { headerName: 'ID Pengajuan', field: 'id_pengajuan' },
+  { headerName: 'Jumlah', field: 'jumlah' },
+  { headerName: 'Tanggal', field: 'tanggal' },
+  { headerName: 'Keterangan', field: 'keterangan' },
+  { headerName: 'Bukti', field: 'bukti', cellRenderer: agLinkCellRenderer },
+  {
+    headerName: 'Tags',
+    field: 'tags',
+    filter: 'agSetColumnFilter',
+    filterParams: {
+      applyMiniFilterWhileTyping: true,
+      values: ['VIP','In Progress','Urgent','Bug','VVIP'],
+    } as ISetFilterParams,
+    cellRenderer: tagsCellRenderer
+  }
+];
+
+const rowData = [
+  {
+    "id": 1,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "VIP"
+  },
+  {
+    "id": 2,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "In Progress"
+  },
+  {
+    "id": 3,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "Urgent"
+  },
+  {
+    "id": 4,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "Bug"
+  },
+  {
+    "id": 5,
+    "id_pengajuan": 123,
+    "jumlah": 2000000,
+    "tanggal": '10/12/22',
+    "keterangan": "Lorem Ipsum Dolor Sit Amet",
+    "bukti": "evidance.jpg",
+    "tags": "VVIP"
+  }
+];
+
+const defaultColDef = {
+  flex: 1,
+  filter: 'agTextColumnFilter',
+};
+
+const gridOptions = {
+  pagination: true,
+  paginationPageSize: 5,
+  rowHeight: 40,
+  // frameworkComponents, // Tambahkan frameworkComponents ke dalam gridOptions
+};
+
+return (
+  <div>
+    <Semua_laporanNavbar />
+    <div class="pengeluaran-container">
+      {/* div untuk mengatur top table pada pemasukan yang terdiri dari judul tabel, search, dan sorting icon */}
+      <div class="pengeluaran-top-table">
+        <div class="tutup-buku-container">
+          <Tutup_buku />
+        </div>
+
+        <div class="search-container">
+          <div class="search-input">
+            <input
+              type="text"
+              class="form-control"
+              id="filter-text-box"
+              placeholder="Search..."
+            />
+            <span class="search-icon">
+              <Icon icon="ic:baseline-search" color="gray" width="16" height="16" />
+            </span>
+          </div>
+          <button class="btn-sort"><Icon icon="gg:sort-za" color="white" width="25" height="25" /></button>
+        </div>
+      </div>
+      <p>Laporan Pengeluaran</p>
+
+      {/* div untuk mengatur tabel kas besar dari ag grid */}
+      <div class="pengeluaran-table">
+        <div class="ag-theme-alpine" style={{ width: '68vw' }}>
+          <AgGridSolid
+            columnDefs={columnDefs}
+            rowData={rowData}
+            defaultColDef={defaultColDef}
+            domLayout='autoHeight'
+            gridOptions={gridOptions}
+          />
+        </div>
+      </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default Pengeluaran;
