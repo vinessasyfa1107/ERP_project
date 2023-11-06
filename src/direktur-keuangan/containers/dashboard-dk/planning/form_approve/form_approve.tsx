11908428b06387fa7bd6e1556a9fcee1244da82c
Vinessa Gabby Asyfa <vgabbyasyfa1107@gmail.com>
2023-11-06 11:55:00 +0700

    berhasil membuat approve direktur keuangan

--- src/direktur-keuangan/containers/dashboard-dk/planning/form_approve/form_approve.tsx
+++ src/direktur-keuangan/containers/dashboard-dk/planning/form_approve/form_approve.tsx
@@ -5,40 +5,114 @@ import { Icon } from '@iconify-icon/solid';
 interface EditPopUpProps {
     OnClose: () => void;
     data: {
-        id: number;
-        entry_ts: Date;
-        description: string;
-        planningtype: string;
-        category: number;
-        amount: number;
+        id: number,
+        entry_ts: string,
+        description: string,
+        planningtype: string,
+        category: number,
+        amount: number,
+        coa_kd: string,
         // Tambahkan properti lain yang sesuai
     };
-    updateStatusButton: (data: object, status: string) => void;
+    // updateStatusButton: (data: object, status: string) => void;
 }
 
 const Form_approve: Component<EditPopUpProps> = (props) => {
 
-    const timestampString = props.data.entry_ts; // "2023-10-23T00:00:00"
-    const dateObject = new Date(timestampString); // Mengonversi ke objek Date
+    const [status, setStatus] = createSignal('');
+    const [timestamp, setTimestamp] = createSignal('');
 
-    // Anda dapat menggunakan dateObject untuk menampilkan tanggal dalam format yang sesuai
-    const formattedDate = dateObject.toLocaleDateString('en-US', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-    });
 
-    const handleApprove = () => {
-        props.updateStatusButton(props.data, 'Approved'); // Call the function to update status as 'approved'
-        props.OnClose(); // Close the popup
+    const handleInputChange = (e) => {
+        const { value } = e.target;
+        setStatus(value);
+        if (value === 'InProgress' || value === 'Rejected') {
+            // Menggunakan timestamp saat ini dalam format ISO 8601
+
+            const currentDate = new Date();
+            const formattedDate = currentDate.toISOString().slice(0, 11);
+
+            const hours = String(currentDate.getHours()).padStart(2, '0');
+            const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+            const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+            const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+            const timestamp = `${formattedDate}${formattedTime}`;
+
+            console.log("tanggal dan waktu: ", timestamp);
+            setTimestamp(timestamp);
+        }
+
+        updateStatus();
     };
 
-    const handleReject = () => {
-        props.updateStatusButton(props.data, 'Rejected'); // Call the function to update status as 'rejected'
-        props.OnClose(); // Close the popup
+    const categoryValueMap = {
+        "Marketing": 1,
+        "Project": 2,
+        "Rutinitas": 3,
+        "Event": 4
     };
 
-    
+    // Fungsi bantuan untuk mendapatkan nilai dari category string
+    function getCategoryValue(category) {
+        return categoryValueMap[category] || 0; // Nilai default jika tidak ada pemetaan
+    }
+
+    // Menggunakan fungsi getCategoryValue untuk mendapatkan nilai
+    const category = props.data.category;
+    const categoryValue = getCategoryValue(category);
+
+
+    const updateStatus = async () => {
+        const updateStatusToSend = {
+            id: props.data.id,
+            entry_ts: timestamp(),
+            coa_kd: props.data.coa_kd,
+            description: props.data.description,
+            planningtype: props.data.planningtype,
+            category: categoryValue,
+            amount: props.data.amount,
+            status: status()
+        }
+        console.log("test", updateStatusToSend);
+
+        try {
+            const response = await fetch(`/api/planning/${(props.data.id)}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    id: props.data.id,
+                    entry_ts: timestamp(),
+                    coa_kd: props.data.coa_kd,
+                    description: props.data.description,
+                    planningtype: props.data.planningtype,
+                    category: categoryValue,
+                    amount: props.data.amount,
+                    status: status()
+                }),
+            });
+
+            if (response.ok) {
+                // Data berhasil diubah, tampilkan alert
+                alert('Data berhasil diubah');
+                props.OnClose();
+                window.location.reload();
+            } else {
+                // Gagal mengubah data, tampilkan pesan kesalahan dari respons
+                const errorMessage = await response.text();
+                alert(`Gagal mengubah data. Pesan kesalahan: ${errorMessage}`);
+                console.error('Gagal mengubah data:', errorMessage);
+            }
+        } catch (error) {
+            // Terjadi kesalahan jaringan atau kesalahan lainnya, tampilkan alert dengan pesan kesalahan
+            alert('Terjadi kesalahan. Silakan coba lagi.');
+            console.error('Terjadi kesalahan:', error);
+        }
+    };
+
+
 
     return (
         <div class="overlay">
@@ -54,7 +128,7 @@ const Form_approve: Component<EditPopUpProps> = (props) => {
 
                             <label>Tanggal</label>
                             <br />
-                            <input type="text" value={formattedDate} readonly />
+                            <input type="text" value={props.data.entry_ts} readonly />
 
 
                             <p>
@@ -109,19 +183,19 @@ const Form_approve: Component<EditPopUpProps> = (props) => {
 
                         <br />
                         <div class="btn-add-acc">
-                            <button style={{
+                            <button value='InProgress' style={{
                                 "background-color": "rgba(132, 103, 255, 0.80)",
                                 "border-radius": "5px",
                                 "width": "7rem",
                                 "height": "2rem",
                                 "margin-right": "30px"
-                            }} onClick={handleApprove}>Approved</button>
-                            <button style={{
+                            }} onClick={handleInputChange}>Approved</button>
+                            <button value='Rejected' style={{
                                 "background-color": "#F56D59",
                                 "border-radius": "5px",
                                 "width": "7rem",
                                 "height": "2rem"
-                            }} onClick={handleReject}>Rejected</button>
+                            }} onClick={handleInputChange}>Rejected</button>
                         </div>
                     </form>
 
