21369e5d1981260ecbccffb2302d11907492786f
Vinessa Gabby Asyfa <vgabbyasyfa1107@gmail.com>
2023-12-21 13:06:23 +0700

    evidence pada pemasukan

--- src/containers/report/pemasukan/pemasukan.tsx
+++ src/containers/report/pemasukan/pemasukan.tsx
@@ -14,22 +14,40 @@ import './pemasukan.css';
 import Tutup_buku from './tutup_buku';
 import { DataIncome } from '../../../api/report/data-income';
 import { useSubNavbarStore } from '../../../store/Navbar/SubNavbarStore';
+import Form_evidence from './form_evidence';
 
 
 const Pemasukan: Component = () => {
-  const [, {changeSubTitleNavbar} ] = useSubNavbarStore();
+  const [, { changeSubTitleNavbar }] = useSubNavbarStore();
+  const [evidence, setEvidence] = createSignal('');
+  const [isEditPopupOpen, setIsEditPopupOpen] = createSignal(false);
+  const [editedData, setEditedData] = createSignal(null);
 
-    onMount(() => {
-        changeSubTitleNavbar("Laporan Pemasukan");
-    })
+  onMount(() => {
+    changeSubTitleNavbar("Laporan Pemasukan");
+  })
 
   const [RowData, setRowData] = createSignal([{}]);
 
   onMount(async () => {
-    const income = await DataIncome ("hallo");
+    const income = await DataIncome("hallo");
     console.log("income", income);
     setRowData(income);
   })
+
+
+  // pop up evidence
+  const showEditPopup = (rowData: any) => {
+    setEditedData(rowData);
+    setIsEditPopupOpen(true);
+    setEvidence(rowData.evidence);
+    console.log("test", evidence());
+  };
+
+  function CloseEditPopUp() {
+    setIsEditPopupOpen(false);
+  }
+
 
   const agLinkCellRenderer = (params: any) => {
     const bukti = params.data.bukti;
@@ -102,66 +120,76 @@ const Pemasukan: Component = () => {
         values: ['VIP', 'In Progress', 'Urgent', 'Bug', 'VVIP'],
       } as ISetFilterParams,
       cellRenderer: tagsCellRenderer
-    }
+    },
+    {
+      field: "transfer", headerName: "", cellRenderer: (params: any) => {
+        return (
+          <div style={{ "justify-content": "center", "align-items": "center", "margin-right": "20px" }}>
+            <button onClick={() => showEditPopup(params.data)} style={{ "background-color": "#6E49E9", "justify-content": "center", "border-radius": "10px", "width": "5.5rem", "height": "2.3rem", "color": "white", "align-items": "center" }}>Evidence &gt</button>
+            {params.value}
+          </div>
+        );
+      }
+    },
   ];
 
-  const rowData = [
-    {
-      "id": 1,
-      "id_pengajuan": 123,
-      "faktur": "KM20231016",
-      "COA": "1-1000",
-      "jumlah": 2000000,
-      "tanggal": '10/12/22',
-      "keterangan": "Lorem Ipsum Dolor Sit Amet",
-      "bukti": "evidance.jpg",
-      "tags": "VIP"
-    },
-    {
-      "id": 2,
-      "id_pengajuan": 123,
-      "faktur": "KM20231016",
-      "COA": "1-1000",
-      "jumlah": 2000000,
-      "tanggal": '10/12/22',
-      "keterangan": "Lorem Ipsum Dolor Sit Amet",
-      "bukti": "evidance.jpg",
-      "tags": "In Progress"
-    },
-    {
-      "id": 3,
-      "id_pengajuan": 123,
-      "faktur": "KM20231016",
-      "COA": "1-1000",
-      "jumlah": 2000000,
-      "tanggal": '10/12/22',
-      "keterangan": "Lorem Ipsum Dolor Sit Amet",
-      "bukti": "evidance.jpg",
-      "tags": "Urgent"
-    },
-    {
-      "id": 4,
-      "id_pengajuan": 123,
-      "faktur": "KM20231016",
-      "COA": "1-1000",
-      "jumlah": 2000000,
-      "tanggal": '10/12/22',
-      "keterangan": "Lorem Ipsum Dolor Sit Amet",
-      "bukti": "evidance.jpg",
-      "tags": "Bug"
-    },
-    {
-      "id": 5,
-      "id_pengajuan": 123,
-      "faktur": "KM20231016",
-      "COA": "1-1000",
-      "jumlah": 2000000,
-      "tanggal": '10/12/22',
-      "keterangan": "Lorem Ipsum Dolor Sit Amet",
-      "bukti": "evidance.jpg",
-      "tags": "VVIP"
-    }
-  ];
+  // const rowData = [
+  //   {
+  //     "id": 1,
+  //     "id_pengajuan": 123,
+  //     "faktur": "KM20231016",
+  //     "COA": "1-1000",
+  //     "jumlah": 2000000,
+  //     "tanggal": '10/12/22',
+  //     "keterangan": "Lorem Ipsum Dolor Sit Amet",
+  //     "bukti": "evidance.jpg",
+  //     "tags": "VIP"
+  //   },
+  //   {
+  //     "id": 2,
+  //     "id_pengajuan": 123,
+  //     "faktur": "KM20231016",
+  //     "COA": "1-1000",
+  //     "jumlah": 2000000,
+  //     "tanggal": '10/12/22',
+  //     "keterangan": "Lorem Ipsum Dolor Sit Amet",
+  //     "bukti": "evidance.jpg",
+  //     "tags": "In Progress"
+  //   },
+  //   {
+  //     "id": 3,
+  //     "id_pengajuan": 123,
+  //     "faktur": "KM20231016",
+  //     "COA": "1-1000",
+  //     "jumlah": 2000000,
+  //     "tanggal": '10/12/22',
+  //     "keterangan": "Lorem Ipsum Dolor Sit Amet",
+  //     "bukti": "evidance.jpg",
+  //     "tags": "Urgent"
+  //   },
+  //   {
+  //     "id": 4,
+  //     "id_pengajuan": 123,
+  //     "faktur": "KM20231016",
+  //     "COA": "1-1000",
+  //     "jumlah": 2000000,
+  //     "tanggal": '10/12/22',
+  //     "keterangan": "Lorem Ipsum Dolor Sit Amet",
+  //     "bukti": "evidance.jpg",
+  //     "tags": "Bug"
+  //   },
+  //   {
+  //     "id": 5,
+  //     "id_pengajuan": 123,
+  //     "faktur": "KM20231016",
+  //     "COA": "1-1000",
+  //     "jumlah": 2000000,
+  //     "tanggal": '10/12/22',
+  //     "keterangan": "Lorem Ipsum Dolor Sit Amet",
+  //     "bukti": "evidance.jpg",
+  //     "tags": "VVIP"
+  //   }
+  // ];
 
   const defaultColDef = {
     flex: 1,
@@ -213,6 +241,12 @@ const Pemasukan: Component = () => {
               gridOptions={gridOptions}
             />
           </div>
+          {isEditPopupOpen() && editedData() && (
+            <Form_evidence
+              evidence={evidence()}
+              OnClose={CloseEditPopUp}
+            />
+          )}
         </div>
       </div>
     </div>
